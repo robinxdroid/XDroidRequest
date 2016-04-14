85331f1ec3c8d7ede4de4a3110b887c92ab74ced
Robin <robinxdroid@gmail.com>
2016-04-14 11:59:37 +0800

    update:解决Android Studio无法打印包含特殊字符的结果问题

--- XDroidRequest/src/com/xdroid/request/ex/MultipartGsonRequest.java
+++ XDroidRequest/src/com/xdroid/request/ex/MultipartGsonRequest.java
@@ -2,6 +2,8 @@ package com.xdroid.request.ex;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
@@ -13,6 +15,9 @@ import com.xdroid.request.response.NetworkResponse;
 import com.xdroid.request.response.Response;
 import com.xdroid.request.utils.CLog;
 import com.xdroid.request.utils.GenericsUtils;
+
+import android.text.TextUtils;
+
 /**
  * Parse the result by "GSON"
  * 
@@ -24,13 +29,12 @@ import com.xdroid.request.utils.GenericsUtils;
 public class MultipartGsonRequest<T> extends MultipartRequest<T> {
 
 	private Type mBeanType;
-	
+
 	public MultipartGsonRequest() {
 		super();
 	}
 
-	public MultipartGsonRequest(RequestCacheConfig cacheConfig, String url, String cacheKey,
-			OnRequestListener<T> onRequestListener) {
+	public MultipartGsonRequest(RequestCacheConfig cacheConfig, String url, String cacheKey, OnRequestListener<T> onRequestListener) {
 		super(cacheConfig, url, cacheKey, onRequestListener);
 		mBeanType = GenericsUtils.getBeanType(onRequestListener);
 	}
@@ -39,8 +43,16 @@ public class MultipartGsonRequest<T> extends MultipartRequest<T> {
 	@Override
 	public Response<T> parseNetworkResponse(NetworkResponse response) {
 		String result = new String(response.data);
-		
-		CLog.d("[Original String Data]:%s",result);
+
+		CLog.d("[Original String Data]:%s", result);
+
+		//Because Android Studio cannot print string contains special characters, so here to filter out the special characters
+		if (!TextUtils.isEmpty(result)) {
+			Pattern CRLF = Pattern.compile("(\r\n|\r|\n|\n\r|\t)");
+			Matcher matcher = CRLF.matcher(result);
+			result = matcher.replaceAll("");
+			CLog.d("[Filter the special characters of the original character data]:%s", result);
+		}
 
 		if (mBeanType.equals(String.class)) {
 			T parseResult = (T) result;
@@ -66,7 +78,7 @@ public class MultipartGsonRequest<T> extends MultipartRequest<T> {
 		}
 		return null;
 	}
-	
+
 	public <X> X fromJsonObject(String json, Type cls) {
 		Gson gson = new Gson();
 		X bean = gson.fromJson(json, cls);
